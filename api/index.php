2315523080cd92098135ad2bcb9aa841ec13a2c0
Suman-1108 <hellosuman29@gmail.com>
2026-09-20 19:25:30 +0530

    Fix file download issue by explicitly setting HTML Content-Type headers

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
 
+// Set HTML content-type header so browsers render the page instead of downloading
+if (!headers_sent()) {
+    header('Content-Type: text/html; charset=UTF-8');
+}
+
 // Forward all requests to CodeIgniter front controller in public/index.php
 require_once __DIR__ . '/../public/index.php';
